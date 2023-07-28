905740e687749176a704860388e7c092404224eb
JokinAbarzua <jokinabarzua22@hotmail.com>
2023-07-28 12:32:38 -0300

    Correcciones

--- src/components/MMFormTextInput/MMFormTextInput.tsx
+++ src/components/MMFormTextInput/MMFormTextInput.tsx
@@ -1,18 +1,17 @@
-import { Input } from '@mui/material';
-import { display, flexbox } from '@mui/system';
-import { FieldError, FieldErrors, FieldValues, RegisterOptions, useForm, useFormContext, UseFormRegister, UseFormRegisterReturn } from 'react-hook-form';
+import { HTMLInputTypeAttribute } from 'react';
+import { RegisterOptions, useFormContext } from 'react-hook-form';
 import "./MMFormTextInput.scss"
 
 interface Props {
   label: string;
   name: string;
-  type: string;
+  type: HTMLInputTypeAttribute;
   options: RegisterOptions;
 }
 
 /**
  * 
- * This component must be wraped in a FormProvider from "react-hook-form" to work properly
+ * This component must be wrapped in a FormProvider from "react-hook-form" to work properly
  * 
  */
 export const MMFormTextInput = (props: Props) => {
@@ -21,8 +20,8 @@ export const MMFormTextInput = (props: Props) => {
   return (
     <>
       <div className='input-container'>
-        <span className='input-title'>{props.label}</span>
-        <input type={props.type.toString().toLowerCase()} {...register(props.name,props.options)} className="input-input"/>
+        <label className='input-title'>{props.label}</label>
+        <input type={props.type} {...register(props.name,props.options)} className="input-input"/>
       </div>
     </>
   );
