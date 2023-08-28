ea20c01786795569457ccd0cbe7629933296f4ad
Tomas <tomasespin12@gmail.com>
2023-08-28 17:57:52 -0300

    Validate nationality and description. Change genre-select route.

--- src/models/Artist.ts
+++ src/models/Artist.ts
@@ -25,9 +25,17 @@ export const artistValidations: Readonly<ModelValidations<Artist>> = {
     maxLength: { value: 128, message: 'El nombre debe ser menor a 128 caracteres' }
   },
   nationality: {
+    required: {
+      value: true,
+      message: 'Debe ingresar la nacionalidad del artista'
+    },
     maxLength: { value: 128, message: 'La nacionalidad debe ser menor a 128 caracteres' }
   },
   description: {
+    required: {
+      value: true,
+      message: 'Debe ingresar la descripción del artista'
+    },
     maxLength: { value: 2048, message: 'La descripción debe ser menor a 2048 caracteres' }
   }
 };
--- src/services/genreService.tsx
+++ src/services/genreService.tsx
@@ -1,14 +1,7 @@
 import axios from 'axios';
 import { Genre } from '../models/Genre';
 
-const genresUrl = `${process.env.REACT_APP_API_URL}/genres`;
 const genresAdminUrl = `${process.env.REACT_APP_API_URL}/admin/genres`;
-
-export async function fetchGenresSelect(): Promise<Genre[]> {
-  const response = await axios.get<Genre[]>(`${genresUrl}/genres_select`);
-
-  return response.data;
-}
 
 export async function createGenre(name: string): Promise<Genre> {
   const body = {
@@ -30,4 +23,10 @@ export async function updateGenre(id: string, name: string): Promise<Genre> {
     }
   };
   return (await axios.put(`${genresAdminUrl}/${id}`, body)).data;
+}
+
+export async function fetchGenresSelect(): Promise<Genre[]> {
+  const response = await axios.get<Genre[]>(`${genresAdminUrl}/genres_select`);
+
+  return response.data;
 }
