ed069ffec0f03c9526ca60d79a392c61b1f223a1
JokinAbarzua <jokinabarzua22@hotmail.com>
2023-09-17 23:19:45 -0300

    Fixes

--- src/components/form/PermissionListInput/PermissionListInput.tsx
+++ src/components/form/PermissionListInput/PermissionListInput.tsx
@@ -64,7 +64,7 @@ export const PermissionListInput = ({ setSelectedPermissions, selectedPermission
                         onChange={handleCeckboxChange}
                         disabled={disabled}
                       />
-                      <span>{perm.action}</span>
+                      <span>{perm.name}</span>
                     </label>
                   );
                 })}
