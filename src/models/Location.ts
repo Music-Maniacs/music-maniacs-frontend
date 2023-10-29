c0ea0d50c423cfc9cf62a0d3e20f2a53ca156399
Tomas <tomasespin12@gmail.com>
2023-10-29 18:03:26 -0300

    Latitud y Longitud obligatorias

--- src/models/Location.ts
+++ src/models/Location.ts
@@ -14,10 +14,18 @@ export interface Location {
 
 export const locationValidations: Readonly<ModelValidations<Location>> = {
   latitude: {
+    required: {
+      value: true,
+      message: 'Debe ingresar la latitud'
+    },
     min: { value: -90, message: 'La latitud debe ser mayor a -90' },
     max: { value: 90, message: 'La latitud debe ser menor a 90' }
   },
   longitude: {
+    required: {
+      value: true,
+      message: 'Debe ingresar la longitud'
+    },
     min: { value: -180, message: 'La longitud debe ser mayor a -180' },
     max: { value: 180, message: 'La longitud debe ser menor a 180' }
   },
